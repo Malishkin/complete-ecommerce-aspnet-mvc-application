b93f19b26c393f8fa2acb1479c5405a27529c6d9
Malishkin <malishkin2002@gmail.com>
2022-03-06 11:15:34 +0200

    Actors Service

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -10,6 +11,9 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options=>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+//Services configuration 
+builder.Services.AddScoped<IActorsService, ActorsService>();
 
 var app = builder.Build();
 
